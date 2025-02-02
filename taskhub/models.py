cfa0a5b6a5cf51716d502dc72d5e92e02e1bac39
taraskhom <khomenkotaras1305@gmail.com>
2025-02-02 18:08:08 +0200

    add models for Position, Worker, Task, and TaskType with relationships

--- taskhub/models.py
+++ taskhub/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Task(models.Model):
+
+    PRIORITY_CHOICES = [
+        ('U', 'Urgent'),
+        ('H', 'High Priority'),
+        ('M', 'Medium Priority'),
+        ('L', 'Low Priority'),
+        ('N', 'Normal'),
+    ]
+
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    deadline = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=1, choices=PRIORITY_CHOICES, default='N')
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(Worker)
